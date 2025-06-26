164ef65406bb8787240054d884d7b07564ebf307
Atharv Sonawane <atharv3256@gmail.com>
2025-06-26 14:00:55 +0530

    Add files via upload

---
+++ threenumbers.java
@@ -0,0 +1,12 @@
+public class threenumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
+
